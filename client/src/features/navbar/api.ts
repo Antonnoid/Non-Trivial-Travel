3bc6cb19322c6c656523c2bb6b3e289bced6fbf5
Anton <antivzzz26@gmail.com>
2023-09-02 21:25:43 +0300

    Поправил пути в запросах, чтоб не склеивались на страницах города

--- client/src/features/navbar/api.ts
+++ client/src/features/navbar/api.ts
@@ -1,15 +1,15 @@
 import {City} from './types/types';
 
 export const fetchCity = async (): Promise<City[]> => {
-  const res = await fetch('api/cities/');
+  const res = await fetch('/api/cities/');
   return res.json();
 };
 export const fetchCityPopular = async (): Promise<City[]> => {
-  const res = await fetch('api/cities/popular');
+  const res = await fetch('/api/cities/popular');
   return res.json();
 };
 export const fetchCityByLetter = async (name: string): Promise<City[]> => {
-  const res = await fetch('api/cities/search/name', {
+  const res = await fetch('/api/cities/search/name', {
     method: 'POST',
     headers: {'Content-type': 'application/json'},
     body: JSON.stringify({name}),
