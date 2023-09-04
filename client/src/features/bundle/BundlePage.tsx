71f6d92c8894b766ba9c71b87c0f302246867098
Antonnoid <izhanton@gmail.com>
2023-09-04 18:28:16 +0300

    checked

---
+++ client/src/features/bundle/BundlePage.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { RootState, useAppDispatch } from '../../redux/store'
+import { useSelector } from 'react-redux'
+import { Bundle } from './type'
+import { Place } from '../place/type'
+import PlaceCard from '../place/PlaceCard'
+import { bundlesInit } from './bundlesSlice'
+
+const BundlePage = ():JSX.Element => {
+  const dispatch = useAppDispatch()
+  const {bundleId} = useParams()
+  const bundles = useSelector((store:RootState) => store.bundles.bundles)
+  const places = useSelector((store: RootState) => store.places.places);
+  let ourBundle;
+  let ourPlaces;
+  if(bundleId){
+    ourBundle = bundles.find((bundle: Bundle) => bundle.id === +bundleId)!!;
+    ourPlaces = places.filter((place: Place) => place.bundleId === +bundleId)
+  }
+  useEffect(()=>{
+dispatch(bundlesInit())
+  },[])
+  return (
+    <div>
+      <h1>{ourBundle?.title}</h1>
+      <h2>{ourBundle?.description}</h2>
+      <div>{ourPlaces?.map((place) => (<PlaceCard place={place} key={place.id}/>))}</div>
+    </div>
+  )
+}
+
+export default BundlePage
