9c4108daf21268f36ae9e9ed5451788788278cca
Anton <antivzzz26@gmail.com>
2023-09-06 17:43:10 +0300

    fix

--- client/src/features/place/FormAdd.tsx
+++ client/src/features/place/FormAdd.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState, useRef} from 'react';
+import {useSelector} from 'react-redux';
 import {RootState, useAppDispatch} from '../../redux/store';
 import {placeAddfromForm} from './placesSlice';
 import DropList from '../droplist/DropList';
-import {useSelector} from 'react-redux';
 import {loadCitiesByLetter, loadCitiesPopular} from '../city/citiesSlice';
 
 export default function FormAdd(): JSX.Element {
