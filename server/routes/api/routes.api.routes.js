143b59b50c9a18cb119e89f315f2af74c237405f
NesterovSA <im.nesterovsa@gmail.com>
2023-09-07 22:15:57 +0300

    Merging issues fixed

--- server/routes/api/routes.api.routes.js
+++ server/routes/api/routes.api.routes.js
@@ -44,7 +44,6 @@ router.post('/:routeId/rating', async (req, res) => {
   }
 });
 
-
 // удаление
 router.delete('/:routeId', async (req, res) => {
   try {
@@ -61,12 +60,22 @@ router.delete('/:routeId', async (req, res) => {
     res.status(403).json({message: 'Ошибка доступа'});
   } catch ({message}) {
     console.log(message);
+  }
+});
 
 router.post('/', async (req, res) => {
   try {
     const {title, description, isPublic, time, userId, cityId, routePlaces} =
       req.body;
-      console.log(title, description, isPublic, userId, time, cityId, routePlaces);
+    console.log(
+      title,
+      description,
+      isPublic,
+      userId,
+      time,
+      cityId,
+      routePlaces
+    );
     const newRoute = await Route.create({
       title,
       description,
@@ -75,15 +84,14 @@ router.post('/', async (req, res) => {
       userId: +userId,
       cityId: +cityId,
     });
-   routePlaces.map(async (placeId, i) => {
-       await Route_place.create({routeId: newRoute.id, placeId, order: i + 1})
+    routePlaces.map(async (placeId, i) => {
+      await Route_place.create({routeId: newRoute.id, placeId, order: i + 1});
     });
- 
+
     res.json(newRoute);
   } catch ({message}) {
-    console.log({message})
-    res.json({message})
-
+    console.log({message});
+    res.json({message});
   }
 });
 
